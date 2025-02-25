b7c24291e9af071c3c09d8c5a1185851a7aabc2f
Orange <razvan.a.marinescu@gmail.com>
2025-02-25 20:05:52 +0200

    v1.0.5

--- src/main/java/com/github/theFrameworkItems/TheFrameworkItems.java
+++ src/main/java/com/github/theFrameworkItems/TheFrameworkItems.java
@@ -24,7 +24,7 @@ public final class TheFrameworkItems extends JavaPlugin {
 			"&b\n\n" +
 				"  █ ▀█▀ █▀▀ █▀▄▀█ █▀\n" +
 				"  █  █  ██▄ █ ▀ █ ▄█\n" +
-				"              &3v&f1.0.4" +
+				"              &3v&f1.0.5" +
 				"\n"
 		));
 	}
